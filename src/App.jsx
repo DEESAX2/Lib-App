3938436e4ff047dcc12ba0bb5f8ddcffed217907
Alexykay <alexisayirebiacquah@gmail.com>
2025-06-09 11:38:00 -0700

    tailwind css added to index css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
   return (
     <>
      <div>
-      <h1>Welcome to My Lib App</h1>
+      <h1 className='text-3xl font-extrabold'>Welcome to My Lib App</h1>
      </div>
     </>
   )
