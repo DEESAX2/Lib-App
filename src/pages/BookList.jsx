f8a7a24dc39872a216947535c85dc3d9a4c79221
Alexykay <alexisayirebiacquah@gmail.com>
2025-06-09 16:05:49 -0700

    added to booklist page

--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,8 +1,49 @@
+import dominicunsplash from "../assets/images/dominicunsplash.jpg"
+
+
+
 export default function BookList() {
   return (
 
       <div>
       <h1>View all Books</h1>
+    
+    <div className="bg-gray-300">
+      <img src="assets/images" alt="dominicunsplash" />
+      <h2 className="font-bold">Best seller</h2>
+      <h3 className="text-blue-600 text-xl">Book title</h3>
+    </div>
+   
+   <div className="bg-gray-300">
+    <img src="assets/images" alt="dominicunsplash" />
+      <h2 className="font-bold">Best seller</h2>
+      <h3 className="text-blue-600 text-xl">Book title</h3>
+  </div>
+
+  <div className="bg-gray-300">
+   <img src="assets/images" alt="dominicunsplash" />
+      <h2 className="font-bold">Best seller</h2>
+      <h3 className="text-blue-600 text-xl">Book title</h3>
+  </div>
+     
+  <div className="bg-gray-300">
+     <img src="assets/images" alt="dominicunsplash" />
+      <h2 className="font-bold">Best seller</h2>
+      <h3 className="text-blue-600 text-xl">Book title</h3>  
+      </div>
+
+      <div className="bg-gray-300">
+     <img src="assets/images" alt="dominicunsplash" />
+      <h2 className="font-bold">Best seller</h2>
+      <h3 className="text-blue-600 text-xl">Book title</h3>
+     </div>
+
+     <div className="bg-gray-300">
+      <img src="assets/images" alt="dominicunsplash" />
+      <h2 className="font-bold">Best seller</h2>
+      <h3 className="text-blue-600 text-xl">Book title</h3> 
+      </div>
+
     </div>
   );
 }
