45d7f366fde57ca41ac98ab59d7d7d513b135577
Manorkie25 <manorkie96@gmail.com>
2025-06-09 16:59:46 -0700

    home update1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer";
+import image from "../assets/images/image.png";
 
 
 export default function Home() {
@@ -7,10 +8,31 @@ export default function Home() {
         <>
             <Navbar />
             <section className="flex flex-col items-center justify-center h-screen">
-                <h1 className="text-6xl text-center font-bold">View Book Page</h1>
+                <img src={image} alt="" />
+                <h1 className="text-6xl text-center font-bold">WELCOME TO BOOK HAVEN</h1>
+
+        <div className="flex flex-col items-center justify-center mt-4">
+     <button>Who is reading</button>
+        <select name="persona" id="persona">
+         <option selected disabled>Select</option> 
+         <option value="kids">Kids</option>
+            <option value="teens">Teens</option>
+            <option value="adults">Adults</option>
+
+</ select>
+<div className="flex flex-row p-4 justify-around space-x-4">
+ <button className="bg-blue-500 text-white px-4 py-2 rounded-xl">KIDS</button>
+<button className="bg-orange-400 text-white px-4 py-2 rounded-xl">KIDS</button>
+            <button className="bg-red-800 text-white px-4 py-2 rounded-xl">TEENS</button>
+            <button className="bg-pink-500 text-white px-4 py-2 rounded-xl">ADULTS</button>
+        </div>
+                        
+        
+
+         </div>
                 
             </section>
-            <Footer />
+            <Footer />n
         </>
-    )
+    );
 }
