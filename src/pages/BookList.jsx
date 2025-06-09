d34223a349d7f74be77de1dcfe9f067cd1b260ce
Alexykay <alexisayirebiacquah@gmail.com>
2025-06-09 16:39:34 -0700

    resolved image source issue

--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -5,41 +5,41 @@ import dominicunsplash from "../assets/images/dominicunsplash.jpg"
 export default function BookList() {
   return (
 
-      <div>
+      <div className="flex grid-cols-3 gap-4 mt-10">
       <h1>View all Books</h1>
     
     <div className="bg-gray-300">
-      <img src="assets/images" alt="dominicunsplash" />
+      <img src={dominicunsplash} alt="dominicunsplash" className="w-30" />
       <h2 className="font-bold">Best seller</h2>
       <h3 className="text-blue-600 text-xl">Book title</h3>
     </div>
    
    <div className="bg-gray-300">
-    <img src="assets/images" alt="dominicunsplash" />
+    <img src={dominicunsplash} alt="dominicunsplash" className="w-30" />
       <h2 className="font-bold">Best seller</h2>
       <h3 className="text-blue-600 text-xl">Book title</h3>
   </div>
 
   <div className="bg-gray-300">
-   <img src="assets/images" alt="dominicunsplash" />
+   <img src={dominicunsplash} alt="dominicunsplash" className="w-30"  />
       <h2 className="font-bold">Best seller</h2>
       <h3 className="text-blue-600 text-xl">Book title</h3>
   </div>
      
   <div className="bg-gray-300">
-     <img src="assets/images" alt="dominicunsplash" />
+     <img src={dominicunsplash} alt="dominicunsplash" className="w-30" />
       <h2 className="font-bold">Best seller</h2>
       <h3 className="text-blue-600 text-xl">Book title</h3>  
       </div>
 
       <div className="bg-gray-300">
-     <img src="assets/images" alt="dominicunsplash" />
+     <img src={dominicunsplash} alt="dominicunsplash" className="w-30"  />
       <h2 className="font-bold">Best seller</h2>
       <h3 className="text-blue-600 text-xl">Book title</h3>
      </div>
 
      <div className="bg-gray-300">
-      <img src="assets/images" alt="dominicunsplash" />
+      <img src={dominicunsplash} alt="dominicunsplash" className="w-30" />
       <h2 className="font-bold">Best seller</h2>
       <h3 className="text-blue-600 text-xl">Book title</h3> 
       </div>
