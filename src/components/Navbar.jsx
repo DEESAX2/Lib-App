6bc4555b470a06e64af3bb5602616a766dba4d96
Ms Sandy A <sandraallotey70@gmail.com>
2025-06-10 16:31:01 +0000

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,36 +4,19 @@ import { Link } from "react-router";
 function Navbar() {
   return (
     <nav className="bg-[#E7E7E7] text-shadow-black fixed top-0 left-0 right-0 shadow-md">
-      <div className="container mx-auto px-4 py-6 flex justify-between items-center">
+      <div className="container mx-auto px-2 py-5 flex justify-between items-center">
         {/* ----LOGO---- */}
         <Link to="#" className="flex items-center space-x-2">
-          <img
-            src={logo}
-            alt="Library Logo"
-            className="h-15 w-35 rounded-full"
-          />
-        </Link>
+          <img src={logo} alt="Library Logo"className="h-15 w-35 rounded-full"/></Link>
 
         {/* ----NAVIGATION LINKS---- */}
         <ul className="flex space-x-6">
-          <li>
-            <Link to="/"className="text-gray-600 hover:text-gray-900 bold transition ">Home</Link>
-          </li>
-          <li>
-            <Link to="/add-book"className="text-gray-600 hover:text-gray-800 transition">AddBook</Link>
-          </li>
-          <li>
-            <Link to="/booklist"className="text-gray-600 hover:text-gray-800 transition">BookList</Link>
-          </li>
-          <li>
-            <Link to="/edit-book"className="text-gray-600 hover:text-gray-800 transition">EditBook</Link>
-          </li>
-          <li>
-            <Link to="/view-book"className="text-gray-600 hover:text-gray-800 transition">ViewBook</Link>
-          </li>
-          <li>
-            <Link to="/contact"className="text-gray-600 hover:text-gray-800 transition">Contact</Link>
-          </li>
+          <li> <Link to="/"className="text-gray-600 hover:text-gray-900 bold transition ">Home</Link></li>
+          <li><Link to="/add-book"className="text-gray-600 hover:text-gray-800 transition">AddBook</Link> </li>
+          <li><Link to="/booklist"className="text-gray-600 hover:text-gray-800 transition">BookList</Link></li>
+          <li><Link to="/edit-book"className="text-gray-600 hover:text-gray-800 transition">EditBook</Link></li>
+          <li><Link to="/view-book/:slug"className="text-gray-600 hover:text-gray-800 transition">ViewBook</Link></li>
+          <li><a href="#footer"className="text-gray-600 hover:text-gray-800 transition">Contact</a> </li>
         </ul>
       </div>
     </nav>
