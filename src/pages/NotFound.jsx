58e09c788cba2ebd99d08f1363453ce95821b1ef
Ms Sandy A <sandraallotey70@gmail.com>
2025-06-10 17:29:17 +0000

    update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,22 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import React from "react";
+import notFound from "../assets/images/notfound.png";
+import { Link } from "react-router";
 
+const NotFound = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center">
+        <img src={notFound} alt="" className="w-full h-110  object-contain" />
+        <h1 className="font-bold text-6xl mt-2 mb-2 ">Oops!</h1>
+        <p className="text-gray-500">
+          The page you are looking for does not exist.
+        </p>
+        <button className="text-blue-500 hover:underline mt-4 inline-block">
+          <Link to="/">Go back to Home</Link>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-
-export default function NotFound() {
-    return (
-        <>
-        <Navbar/>
-        <h1>404-page Not Found</h1>
-        </>
-    )
-}
+export default NotFound;
