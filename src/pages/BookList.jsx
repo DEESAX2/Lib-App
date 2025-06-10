f6439dbe01e5efa2193ecc077b3784e98839947a
Alexykay <alexisayirebiacquah@gmail.com>
2025-06-10 11:15:33 -0700

    Worked on booklist page

--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,49 +1,56 @@
-import dominicunsplash from "../assets/images/dominicunsplash.jpg"
-
+// import dominicunsplash from "../assets/images/dominicunsplash.jpg"
+import arguing from "../assets/images/arguing.jpg"
+import artoflogic from "../assets/images/artoflogic.jpg"
+import idiotbrain from "../assets/images/idiotbrain.jpg"
+import influencebias from "../assets/images/influencebias.jpg"
+import standout from "../assets/images/standout.jpg"
 
 
 export default function BookList() {
   return (
 
-      <div className="flex grid-cols-3 gap-4 mt-10">
-      <h1>View all Books</h1>
+    <section>
     
-    <div className="bg-gray-300">
-      <img src={dominicunsplash} alt="dominicunsplash" className="w-30" />
+    <div><h1 className="text-2xl font-semibold text-center m-10">View all Books</h1></div>
+    
+     <div className="flex grid-cols-3 gap-4 m-10">
+      {/* <div className="bg-gray-300">
+      <img src={dominicunsplash} alt="dominicunsplash" className="w-60" />
       <h2 className="font-bold">Best seller</h2>
       <h3 className="text-blue-600 text-xl">Book title</h3>
-    </div>
-   
-   <div className="bg-gray-300">
-    <img src={dominicunsplash} alt="dominicunsplash" className="w-30" />
-      <h2 className="font-bold">Best seller</h2>
-      <h3 className="text-blue-600 text-xl">Book title</h3>
-  </div>
+    </div> */}
 
-  <div className="bg-gray-300">
-   <img src={dominicunsplash} alt="dominicunsplash" className="w-30"  />
-      <h2 className="font-bold">Best seller</h2>
-      <h3 className="text-blue-600 text-xl">Book title</h3>
-  </div>
-     
-  <div className="bg-gray-300">
-     <img src={dominicunsplash} alt="dominicunsplash" className="w-30" />
-      <h2 className="font-bold">Best seller</h2>
-      <h3 className="text-blue-600 text-xl">Book title</h3>  
+      <div className="bg-gray-300">
+        <img src={arguing} alt="arguing" className="w-60" />
+        <h2 className="font-bold">Best seller</h2>
+        <h3 className="text-blue-600 text-xl">Book title</h3>
       </div>
 
       <div className="bg-gray-300">
-     <img src={dominicunsplash} alt="dominicunsplash" className="w-30"  />
-      <h2 className="font-bold">Best seller</h2>
-      <h3 className="text-blue-600 text-xl">Book title</h3>
-     </div>
+        <img src={artoflogic} alt="artoflogic" className="w-60" />
+        <h2 className="font-bold">Best seller</h2>
+        <h3 className="text-blue-600 text-xl">Book title</h3>
+      </div>
 
-     <div className="bg-gray-300">
-      <img src={dominicunsplash} alt="dominicunsplash" className="w-30" />
-      <h2 className="font-bold">Best seller</h2>
-      <h3 className="text-blue-600 text-xl">Book title</h3> 
+      <div className="bg-gray-300">
+        <img src={idiotbrain} alt="idiotbrain" className="w-60" />
+        <h2 className="font-bold">Best seller</h2>
+        <h3 className="text-blue-600 text-xl">Book title</h3>
+      </div>
+
+      <div className="bg-gray-300">
+        <img src={influencebias} alt="influencebias" className="w-60" />
+        <h2 className="font-bold">Best seller</h2>
+        <h3 className="text-blue-600 text-xl">Book title</h3>
+      </div>
+
+      <div className="bg-gray-300">
+        <img src={standout} alt="standout" className="w-60" />
+        <h2 className="font-bold">Best seller</h2>
+        <h3 className="text-blue-600 text-xl">Book title</h3>
       </div>
 
     </div>
+    </section>
   );
 }
--- src/pages/ViewBook.jsx
+++ src/pages/ViewBook.jsx
@@ -6,8 +6,19 @@ export default function ViewBook() {
     return (
         <>
             <Navbar />
-            <section className="flex flex-col items-center justify-center h-screen">
-                <h1 className="text-6xl text-center font-bold">View Book Page</h1>
+            <section>
+                <h1>View Book Page</h1>
+
+                <h2 className="m-10">Book List</h2>
+                
+                <div>
+                    <ul className="flex flex-row">
+                    <li>Reading </li>
+                     <li> Available</li>   
+                    <li>Completed</li>
+                    <li> Borrow</li>
+                </ul>
+                </div>
                 
             </section>
             <Footer />
