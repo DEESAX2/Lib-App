1667a0e34d75293133e4373123ad389ab5d5bea5
Manorkie25 <manorkie96@gmail.com>
2025-06-10 10:41:44 -0700

    update3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer";
-import image from "../assets/images/image.png";
+import image from "../assets/images/libapp.jpeg";
 
 
 export default function Home() {
@@ -8,8 +8,8 @@ export default function Home() {
         <>
             <Navbar />
             <section className="flex flex-col items-center justify-center h-screen">
-                <img src={image} alt="" />
-                <h1 className="text-6xl text-center font-bold">WELCOME TO BOOK HAVEN</h1>
+                <img src={image} alt=""  className="w-[100%]"/>
+                <h1 className="text-6xl text-center font-bold">WELCOME TO BOOKS HAVEN</h1>
 
         <div className="flex flex-col items-center justify-center mt-4">
      <button>Who is reading</button>
@@ -22,9 +22,8 @@ export default function Home() {
 </ select>
 <div className="flex flex-row p-4 justify-around space-x-4">
  <button className="bg-blue-500 text-white px-4 py-2 rounded-xl">KIDS</button>
-<button className="bg-orange-400 text-white px-4 py-2 rounded-xl">KIDS</button>
-            <button className="bg-red-800 text-white px-4 py-2 rounded-xl">TEENS</button>
-            <button className="bg-pink-500 text-white px-4 py-2 rounded-xl">ADULTS</button>
+<button className="bg-red-800 text-white px-4 py-2 rounded-xl">TEENS</button>
+ <button className="bg-pink-500 text-white px-4 py-2 rounded-xl">ADULTS</button>
         </div>
                         
         
